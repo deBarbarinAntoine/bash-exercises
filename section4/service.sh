e75fc0ef561d6f47644b38cfe9d0abe6c23b31d1
deBarbarinAntoine <antoine.debarbarin@ynov.com>
2023-12-05 17:54:31 +0100

    finished service.sh
    maybe not totally finished. I need to test it after reboot.

--- section4/service.sh
+++ section4/service.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/usr/bin/env bash
 #
 #Created by Antoine de Barbarin, December, 5th 2023
 #This script takes a service as parameter and checks if it exists, if it does, it checks if it is running, and if not, it turns it down.
@@ -8,7 +8,31 @@ usage() {
 	echo "Add -h or --help parameters to view this help."
 }
 
-if [[ " $@ " =~ " -h " ]] || [[ " $@ " =~ " --help " ]] ; then
+activate() {
+  echo "Activating service $1..."
+  if [ "$(whoami)" != "root" ]
+  then
+  sudo systemctl start "$1"
+  else
+    systemctl start "$1"
+  fi
+  echo "Service $1 activated successfully!"
+  exit 0
+}
+
+deactivate() {
+  echo "Deactivating service $1..."
+  if [ "$(whoami)" != "root" ]
+  then
+  sudo systemctl stop "$1"
+  else
+    systemctl stop "$1"
+  fi
+  echo "Service $1 deactivated successfully!"
+  exit 0
+}
+
+if [[ " $* " =~ " -h " ]] || [[ " $* " =~ " --help " ]] ; then
 	usage
 	exit 0
 fi
@@ -17,25 +41,31 @@ if [[ $# != 1 ]]
 then
     usage
     exit 1
-elif [ $(systemctl list-unit-files --type=service | grep $1 ) ]
+elif  systemctl list-unit-files --type service | grep -q "$1"
 then
-    if [ $(systemctl is-active $1) ]
+    if [[ $(systemctl is-active "$1") ]]
     then
-        while [[ ! answer=~^[YyNn]$ ]]
+        while [[ ! $answer =~ ^[YyNn]$ ]]
         do
             read -p "Service $1 is active, do you want to deactivate it? [Y/n] " answer
         done
-        if [[ answer=~^[Yy]$ ]]
-        then
-            echo "Deactivating service $1 ..."
-            systemctl stop $1
-            exit 0
-        else
-            echo "Nothing was done"
-            exit 0
-        fi
+        case $answer in
+          [Yy] ) deactivate "$@";;
+          [Nn] ) echo "Nothing was done"; exit 0;;
+        esac
     else
-        while [[ ! answer=~^[YyNn]$ ]]
+        while [[ ! $answer =~ ^[YyNn]$ ]]
         do
             read -p "Service $1 is inactive, do you want to activate it? [Y/n] " answer
+        done
+        case $answer in
+          [Yy] ) activate "$@";;
+          [Nn] ) echo "Nothing was done"; exit 0;;
+        esac
+    fi
+else
+    echo "Service $1 hasn't been found!"
+    exit 1
+fi
+
 
